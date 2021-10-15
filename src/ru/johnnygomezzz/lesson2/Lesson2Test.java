95a8bc94288450f701009e4165d3ea25712acba8
JohnnyGomezzz <seldesign@mail.ru>
2021-10-15 14:08:54 +0300

    lesson 2 hw completed

--- src/ru/johnnygomezzz/lesson2/Lesson2Test.java
+++ src/ru/johnnygomezzz/lesson2/Lesson2Test.java
@@ -3,16 +3,17 @@ package ru.johnnygomezzz.lesson2;
 public class Lesson2Test {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new MyLinkedList<>();
-        linkedList.add(3);
+        ArrayList<String> arrayList = new MyArrayList<>();
+
         linkedList.add(8);
         linkedList.add(6);
-        linkedList.add(567);
         linkedList.add(89);
         linkedList.insertFirstElement(33);
         linkedList.insertFirstElement(44);
         linkedList.insertFirstElement(55);
 
         linkedList.display();
+
         System.out.println(linkedList.size());
         System.out.println(linkedList.contains(8));
 
@@ -21,6 +22,24 @@ public class Lesson2Test {
         System.out.println(linkedList.getLastElement());
 
         linkedList.removeLastElement();
+        linkedList.remove(33);
+
         linkedList.display();
+
+        System.out.println("---------------------------------\n");
+
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+
+        arrayList.display();
+
+        System.out.println(arrayList.get(1));
+
+        arrayList.remove(2);
+
+        arrayList.display();
+
+        System.out.println(arrayList.size());
     }
 }
